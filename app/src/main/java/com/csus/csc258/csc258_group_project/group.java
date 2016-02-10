c89654d54aad9bc316bed1b456e6602fe6171abd
benw408701 <bwhite@mlhc.com>
2016-02-09 19:07:16 -0800

    Adding event handlers

--- app/src/main/java/com/csus/csc258/csc258_group_project/group.java
+++ app/src/main/java/com/csus/csc258/csc258_group_project/group.java
@@ -5,14 +5,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
- * Created by Yulong on 2016/2/4.
+ * Group Editor Fragment
+ * @author bwhite
  */
-public class group extends Fragment {
+public class group extends Fragment implements View.OnClickListener {
     View root_view;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         root_view = inflater.inflate(R.layout.content_group,container, false);
+
+        Button b = (Button) root_view.findViewById(R.id.btnCreate);
+        b.setOnClickListener(this);
+
         return root_view;
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
