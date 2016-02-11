bbb18d21dc22f998f581f90c1eaa8a6a148fe980
benw408701 <bwhite@mlhc.com>
2016-02-10 20:07:00 -0800

    Was able to get the create group button working

--- app/src/main/java/com/csus/csc258/csc258_group_project/group.java
+++ app/src/main/java/com/csus/csc258/csc258_group_project/group.java
@@ -2,10 +2,13 @@ package com.csus.csc258.csc258_group_project;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearSmoothScroller;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Group Editor Fragment
@@ -26,5 +29,33 @@ public class group extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
+        switch (v.getId()) {
+            // The "Create new Group" button was pressed
+            case R.id.btnCreate:
+                createGroup();
+                break;
+        }
+    }
+
+    private void createGroup() {
+        // Get the layout for the list of views
+        LinearLayout lGroupList = (LinearLayout) root_view.findViewById(R.id.llGroupList);
+
+        // Create a new row
+        LinearLayout lNewGroup = new LinearLayout(root_view.getContext());
+        lNewGroup.setOrientation(LinearLayout.HORIZONTAL);
+
+        // Create a new group name
+        TextView txtGroupName = new TextView(root_view.getContext());
+        txtGroupName.setText("New Group");
+        lNewGroup.addView(txtGroupName);
+
+        // Create a new delete button
+        Button bDelete = new Button(root_view.getContext());
+        bDelete.setText("Delete");
+        lNewGroup.addView(bDelete);
+
+        // Add the new group to the list of groups
+        lGroupList.addView(lNewGroup);
     }
 }
