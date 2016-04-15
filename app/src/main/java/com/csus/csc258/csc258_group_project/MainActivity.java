62418dde429dcd853a53fc651a8c1b7f099aa5e9
Yulonggg <dyl1988123@gmail.com>
2016-04-15 06:48:56 -0600

    1

--- app/src/main/java/com/csus/csc258/csc258_group_project/MainActivity.java
+++ app/src/main/java/com/csus/csc258/csc258_group_project/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.FragmentManager;
 import android.content.BroadcastReceiver;
 import android.content.DialogInterface;
 import android.content.IntentFilter;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -27,7 +28,26 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.DriveContents;
+import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.DriveId;
+import com.google.android.gms.drive.DriveResource;
+import com.google.android.gms.drive.Metadata;
+import com.google.android.gms.drive.MetadataChangeSet;
+
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 
 import java.util.ArrayList;
@@ -37,7 +57,8 @@ import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, TextDialogBox.TextDialogListener {
+        implements NavigationView.OnNavigationItemSelectedListener, TextDialogBox.TextDialogListener, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
     public static final String MyPREFERENCES = "MyPrefs" ;
     public static String device_id ;
@@ -55,6 +76,13 @@ public class MainActivity extends AppCompatActivity
 
     // Collection of groups
     private List<Group> mGroups;
+    // Google drive parameters
+    private GoogleApiClient googleApiClient;
+    private File textFile;
+    public static String drive_id;
+    public static DriveId driveID;
+    private static final int REQUEST_CODE = 101;
+
 private List<GroupFile> mGroupFiles;
     public List<String> groupNamesList()
     {
@@ -129,6 +157,16 @@ private List<GroupFile> mGroupFiles;
         File projDir = new File(dirPath);
         if (!projDir.exists())
             projDir.mkdirs();
+        // the text file in our device's Download folder
+        textFile = new File(getFilesDir().getAbsolutePath() + File.separator + device_id
+                + File.separator + "test.txt");
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Drive.API)
+                .addScope(Drive.SCOPE_FILE)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        googleApiClient.connect();
 
         displayView(R.id.nav_group);
 
@@ -310,6 +348,114 @@ private List<GroupFile> mGroupFiles;
                         manager.getConnectionInfo().getMacAddress()));
                 displayView(R.id.nav_group);
             }
+        }
+    }
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "in onConnected() - we're connected, let's do the work in the background...");
+        Drive.DriveApi.newDriveContents(googleApiClient)
+
+                .setResultCallback(driveContentsCallback);
+    }
+    /*callback on getting the drive contents, contained in result*/
+    final private ResultCallback<DriveApi.DriveContentsResult> driveContentsCallback = new
+            ResultCallback<DriveApi.DriveContentsResult>() {
+                @Override
+                public void onResult(DriveApi.DriveContentsResult result) {
+                    if (!result.getStatus().isSuccess()) {
+                        Log.i(TAG, "Error creating new file contents");
+                        return;
+                    }
+                    final DriveContents driveContents = result.getDriveContents();
+                    new Thread() {
+                        @Override
+                        public void run() {
+                            OutputStream outputStream = driveContents.getOutputStream();
+                            addTextfileToOutputStream(outputStream);
+                            MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
+                                    .setTitle("testFile")
+                                    .setMimeType("text/plain")
+                                    .setDescription("This is a text file uploaded from device")
+                                    .setStarred(true).build();
+                            Drive.DriveApi.getRootFolder(googleApiClient)
+                                    .createFile(googleApiClient, changeSet, driveContents)
+                                    .setResultCallback(fileCallback);
+                        }
+
+                    }.start();
+                }
+            };
+    /*get input stream from text file, read it and put into the output stream*/
+    private void addTextfileToOutputStream(OutputStream outputStream) {
+        Log.i(TAG, "adding text file to outputstream...");
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        try {
+            BufferedInputStream inputStream = new BufferedInputStream(
+                    new FileInputStream(textFile));
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            Log.i(TAG, "problem converting input stream to output stream: " + e);
+            e.printStackTrace();
+        }
+    }
+    /*callback after creating the file, can get file info out of the result*/
+    final private ResultCallback<DriveFolder.DriveFileResult> fileCallback = new
+            ResultCallback<DriveFolder.DriveFileResult>() {
+                @Override
+                public void onResult(DriveFolder.DriveFileResult result) {
+                    if (!result.getStatus().isSuccess()) {
+                        Log.i(TAG, "Error creating the file");
+                        Toast.makeText(MainActivity.this,"Error adding file to Drive", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    Log.i(TAG, "File added to Drive");
+                    Log.i(TAG, "Created a file with content: "
+                            + result.getDriveFile().getDriveId());
+                    Toast.makeText(MainActivity.this,
+                            "Backup finished", Toast.LENGTH_SHORT).show();
+                    final PendingResult<DriveResource.MetadataResult> metadata
+                            = result.getDriveFile().getMetadata(googleApiClient);
+                    metadata.setResultCallback(new ResultCallback<DriveResource.MetadataResult>() {
+                        @Override
+                        public void onResult(DriveResource.MetadataResult metadataResult) {
+                            Metadata data = metadataResult.getMetadata();
+                            Log.i(TAG, "Title: " + data.getTitle());
+                            drive_id = data.getDriveId().encodeToString();
+                            Log.i(TAG, "DrivId: " + drive_id);
+                            driveID = data.getDriveId();
+                            Log.i(TAG, "Description: " + data.getDescription().toString());
+                            Log.i(TAG, "MimeType: " + data.getMimeType());
+                            Log.i(TAG, "File size: " + String.valueOf(data.getFileSize()));
+                        }
+                    });
+                }
+            };
+    public void onConnectionSuspended(int cause) {
+        switch (cause) {
+            case 1:
+                Log.i(TAG, "Connection suspended - Cause: " + "Service disconnected");
+                break;
+            case 2:
+                Log.i(TAG, "Connection suspended - Cause: " + "Connection lost");
+                break;
+            default:
+                Log.i(TAG, "Connection suspended - Cause: " + "Unknown");
+                break;
+        }
+    }
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i(TAG, "Connection failed");
+        if (!result.hasResolution()) {
+            GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), this, 0).show();
+            return;
+        }
+        try {
+            Log.i(TAG, "trying to resolve the Connection failed error...");
+            result.startResolutionForResult(this, REQUEST_CODE);
+        } catch (IntentSender.SendIntentException e) {
+            Log.e(TAG, "Exception while starting resolution activity", e);
         }
     }
 }
