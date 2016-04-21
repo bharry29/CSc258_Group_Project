a26647ebe8571a2fa4365b18988769b48886e0a6
bharry29 <bharry29@gmail.com>
2016-04-21 12:26:23 -0700

    Merge branch 'master' of C:\Users\bharr\AndroidStudioProjects\CSc258_Group_Project with conflicts.

--- app/src/main/java/com/csus/csc258/csc258_group_project/GroupFileView.java
+++ app/src/main/java/com/csus/csc258/csc258_group_project/GroupFileView.java
@@ -1,7 +1,9 @@
 package com.csus.csc258.csc258_group_project;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,8 +12,13 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileWriter;
 import java.util.*;
 
 /**
@@ -22,7 +29,6 @@ public class GroupFileView extends Fragment implements View.OnClickListener {
 
     private ArrayList<Button> bDeleteButtons;
     private Group grp;
-    //private EditText addTextToFile;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         root_view = inflater.inflate(R.layout.content_file, container, false);
@@ -33,13 +39,14 @@ public class GroupFileView extends Fragment implements View.OnClickListener {
 
         MainActivity activity = (MainActivity) getActivity();
         for (GroupFile f : activity.getGroupFiles()) {
-            createFile(f.getFileName(), f.getFileId());
+            createGroupFile(f);
         }
         return root_view;
     }
 
     @Override
     public void onClick(View v) {
+        final MainActivity activity = (MainActivity) getActivity();
 
         // The "Add File" button was pressed
         if (v.getId() == R.id.addfilebtn) {
@@ -55,13 +62,14 @@ public class GroupFileView extends Fragment implements View.OnClickListener {
                 Button b = (Button) v;
                 // Delete button was pressed
                 if (b.getText().equals(getString(R.string.delfilebtn)))
+                    activity.deleteGroupFile(f);
                     //Delete the file
                     ;
             }
         }
     }
 
-    private void createFile(String filename, int id) {
+    private void createGroupFile(GroupFile gf) {
         // Get the layout for the list of files
         LinearLayout lFileList = (LinearLayout) root_view.findViewById(R.id.llFileList);
 
@@ -71,24 +79,42 @@ public class GroupFileView extends Fragment implements View.OnClickListener {
 
         // Create a new text file name
         TextView txtFileName = new TextView(root_view.getContext());
-        txtFileName.setText(filename);
+        txtFileName.setText(gf.getFileName());
+        writeFileOnInternalStorage(getActivity(),gf.getFileName(),"Sample Body");
         lNewFile.addView(txtFileName);
 
-
-        // Create a new Text File
-        EditText addTextToFile = (EditText)new EditText(root_view.getContext());
-        addTextToFile.setText("Enter Your Text here");
-        lNewFile.addView(addTextToFile);
 
         // Create a new delete button
         Button bDelete = new Button(root_view.getContext());
         bDelete.setText(getString(R.string.delfilebtn));
-        bDelete.setId(id);
+        bDelete.setId(gf.getFileId());
         bDelete.setOnClickListener(this);
         bDeleteButtons.add(bDelete);
         lNewFile.addView(bDelete);
 
         // Add the new file to the list of files
         lFileList.addView(lNewFile);
+    }
+
+    public void writeFileOnInternalStorage(Context mcoContext,String sFileName, String sBody){
+        File samplefiledir = new File(mcoContext.getFilesDir(),"sampledata");
+        Boolean write_successful = false;
+        if(!samplefiledir.exists()){
+            samplefiledir.mkdir();
+        }
+        try{
+            File samplefile = new File(samplefiledir, sFileName+ ".txt");
+            FileWriter writer = new FileWriter(samplefile);
+            writer.append(sBody);
+            writer.flush();
+            writer.close();
+            write_successful = true;
+            Toast.makeText(this.getActivity().getApplicationContext(), "Sample File Created Successfully in : " + samplefiledir + "\t with the name:\t"+sFileName , Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e){
+            Toast.makeText(this.getActivity().getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.e("ERROR:---", "Could not write file" + e.getMessage());
+            write_successful = false;
+        }
     }
 }
